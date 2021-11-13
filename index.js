760e2d00a97f9ffa03e3b765b59a0f3ef40dd02e
MaruhknenkoDaniel <78545670+MaruhknenkoDaniel@users.noreply.github.com>
2021-11-13 15:06:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-//(function(){
-    const header = document.querySelector('.header');
-    window.onscroll = () => {
-        if(window.pageYOffset > 50){
-            header.classList.add('header_active');
-        }
-        else {
-            header.classList.remove('header_active');
-        }
-    }    
-}
-());//
+
